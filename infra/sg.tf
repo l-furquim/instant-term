e46068f950ae4475f353373cdcb5172372fd5402
furqas <furquimmsw@gmail.com>
2025-07-31 20:00:29 -0300

    fix tcp typo miss

--- infra/sg.tf
+++ infra/sg.tf
@@ -1,22 +1,22 @@
 resource "aws_security_group" "server_ecs_tasks_sg" {
-  name = "instant-term-server-ecs-taks-sg"
+  name        = "instant-term-server-ecs-taks-sg"
   description = "Allow inbound traffic from 9090 port"
-  vpc_id = aws_vpc.main.id
+  vpc_id      = aws_vpc.main.id
 
   ingress {
-    from_port = 9090
-    to_port = 9090
-    protocol = "tpc"
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
+
   tags = {
     Name        = "instant-term-server-ecs-tasks-sg"
     Environment = var.environment
@@ -24,24 +24,24 @@ resource "aws_security_group" "server_ecs_tasks_sg" {
 }
 
 resource "aws_security_group" "cli_ecs_tasks_sg" {
-  name = "instant-term-cli-ecs-taks-sg"
+  name        = "instant-term-cli-ecs-taks-sg"
   description = "Allow inbound traffic from http port"
-  vpc_id = aws_vpc.main.id
+  vpc_id      = aws_vpc.main.id
 
   ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tpc"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
+
   tags = {
     Name        = "instant-term-cli-ecs-tasks-sg"
     Environment = var.environment
